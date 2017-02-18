6494e6984949df1ed31f06d4197ac4dd92eaa5ad
Alireza Mehrsina <mehr30na@gmail.com>
2017-02-18 14:05:52 +0330

    update client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  {    path: 'student',    component: StudentComponent  },
+
+
+  
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
+import {CrudService} from './crud.service';
+
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +24,7 @@ import { HeaderComponent } from './header/header.component';
     HttpModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CrudService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
